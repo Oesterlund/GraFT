b9fc67da0beb372e2885cad5eb7d22101a5c3d09
Arne Neumann <github@arne.cl>
2024-04-03 16:35:14 +0200

    regression tests: compare all CSV files and posL.gpickle

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -2,6 +2,7 @@ import os
 import filecmp
 import hashlib
 import shutil
+import pickle
 
 import numpy as np
 import skimage.io as io
@@ -34,28 +35,41 @@ def test_env():
                size=6, eps=200, thresh_top=0.5, sigma=1.0, small=50.0,
                angleA=140, overlap=4, max_cost=100,
                name_cell='in silico time')
-    
+
     # Provide the paths that the test_regression function needs.
-    yield test_output_dir, expected_output_dir 
+    yield test_output_dir, expected_output_dir
 
     # Teardown test environment
     shutil.rmtree(test_output_dir)
 
-
-def test_regression(test_env):
+def test_regression_csv_files(test_env):
     output_dir, expected_dir = test_env
-    
-    generated_file = os.path.join(output_dir, "tracked_filaments_info.csv")
-    expected_file = os.path.join(expected_dir, "tracked_filaments_info.csv")
-    
-    # Load the files into pandas DataFrames
-    generated_df = pd.read_csv(generated_file)
-    expected_df = pd.read_csv(expected_file)
-    
-    # Using pandas to compare the DataFrames with a tolerance for floating point errors
-    try:
-        pd.testing.assert_frame_equal(generated_df, expected_df, check_dtype=False, atol=1e-5)
-    except AssertionError as e:
-        raise AssertionError(f"tracked_filaments_info.csv content does not match expected output. {e}")
 
-    # TODO: Extend this to compare all expected files.
+    # use pandas to compare CSV files with a tolerance for floating point errors
+    for csv_fname in ("tracked_filaments_info.csv", "value_per_frame.csv"):
+        generated_file = os.path.join(output_dir, csv_fname)
+        expected_file = os.path.join(expected_dir, csv_fname)
+
+        generated_df = pd.read_csv(generated_file)
+        expected_df = pd.read_csv(expected_file)
+
+        try:
+            pd.testing.assert_frame_equal(generated_df, expected_df, check_dtype=False, atol=1e-5)
+        except AssertionError as e:
+            raise AssertionError(f"{csv_fname} does not match expected output. {e}")
+
+def test_posL_pickle_file(test_env):
+    output_dir, expected_dir = test_env
+
+    fname = 'posL.gpickle'
+    with open(os.path.join(output_dir, fname), 'rb') as f:
+        generated_posL = pickle.load(f)
+    with open(os.path.join(expected_dir, fname), 'rb') as f:
+        expected_posL = pickle.load(f)
+
+    assert len(generated_posL) == len(expected_posL), \
+        "{fname}: number of frames does not match expected output."
+
+    for frame_index, (generated_frame_pos, expected_frame_pos) in enumerate(zip(generated_posL, expected_posL)):
+        assert np.array_equal(generated_frame_pos, expected_frame_pos), \
+        f"Node positions don't match for frame {frame_index}."
