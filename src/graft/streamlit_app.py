a4fdb1d000b50cec48ce99d424e94b8e51272c77
Arne Neumann <github@arne.cl>
2024-07-02 19:12:55 +0200

    WIP: fix ioerror
    AttributeError: '_io.BufferedReader' object has no attribute 'getvalue'
    Traceback:
    
    File "/home/arne/repos/GraFT/venv-py310/lib/python3.10/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 584, in _run_script
        exec(code, module.__dict__)
    File "/home/arne/repos/GraFT/src/graft/streamlit_app.py", line 266, in <module>
        main()
    File "/home/arne/repos/GraFT/src/graft/streamlit_app.py", line 259, in main
        run_analysis(uploaded_file, params)
    File "/home/arne/repos/GraFT/src/graft/streamlit_app.py", line 129, in run_analysis
        bytes_data = BytesIO(uploaded_file.getvalue())

--- src/graft/streamlit_app.py
+++ src/graft/streamlit_app.py
@@ -203,45 +203,64 @@ def main():
     title_str = 'GraFT: Graph of Filaments over Time'
     st.set_page_config(page_title=title_str)
     st.title(title_str)
-    description = """**GraFT** is a tool designed for identifying and tracking filamentous structures from 2D time-series image data. The source code is available [here](https://github.com/Oesterlund/GraFT)."""
-    st.markdown(description)
-    
-    uploaded_file = st.file_uploader("Upload TIFF file", type=['tif', 'tiff'], on_change=reset_session_state)
+
+    with st.container(border=True):
+        description = """**GraFT** is a tool designed for identifying and tracking filamentous structures from 2D time-series image data. The source code is available [here](https://github.com/Oesterlund/GraFT)."""
+        st.markdown(description)
+
+    image_source = st.radio(
+        "Choose image source:",
+        ("Upload your own image", "Use example image")
+    )
+
+    if image_source == "Upload your own image":
+        uploaded_file = st.file_uploader("Upload TIFF file", type=['tif', 'tiff'], on_change=reset_session_state)
+    else:
+        example_choice = st.selectbox(
+            "Choose an example image:",
+            ("still_image.tif", "timeseries.tif")
+        )
+        example_path = get_tiff_path(example_choice)
+        uploaded_file = open(example_path, "rb")
 
     # Sidebar for configuration
     st.sidebar.title("Configuration")
     params = {  # all slider values: min, max, default
         "Smoothing": st.sidebar.select_slider(
-			'Smoothing', options=[0, 0.5, 1, 1.5, 2, 2.5, 3], value=1.0, on_change=reset_session_state,
-			help='Applies Gaussian blur to fix potential breakage from noisy image data. Keep this value low (1-2) for most cases. If you have very noisy data, try setting this value higher.'),
-		"Noisy Objects": st.sidebar.slider(
-			'Noisy Objects', 10.0, 100.0, 50.0, on_change=reset_session_state,
-			help='Removes small objects by eliminating discrete clusters of pixels below the specified threshold. For non-noisy images with fine tubular structures, use a low value (around 10). For noisier data, use higher values (50 or above).'),
+            'Smoothing', options=[0, 0.5, 1, 1.5, 2, 2.5, 3], value=1.0, on_change=reset_session_state,
+            help='Applies Gaussian blur to fix potential breakage from noisy image data. Keep this value low (1-2) for most cases. If you have very noisy data, try setting this value higher.'),
+        "Noisy Objects": st.sidebar.slider(
+            'Noisy Objects', 10.0, 100.0, 50.0, on_change=reset_session_state,
+            help='Removes small objects by eliminating discrete clusters of pixels below the specified threshold. For non-noisy images with fine tubular structures, use a low value (around 10). For noisier data, use higher values (50 or above).'),
         "Minimum Angle": st.sidebar.slider(
-			'Minimum Angle', 100, 180, 140, on_change=reset_session_state,
-			help='Used for tracing filamentous structures. The tracing is done via depth-first search, constrained by angles based on the spatial positions of the nodes. This parameter sets the minimum allowed angle for visiting nodes during the search. If the user-defined angle is lower than the calculated angle, the user-defined minimum angle is used.'),
+            'Minimum Angle', 100, 180, 140, on_change=reset_session_state,
+            help='Used for tracing filamentous structures. The tracing is done via depth-first search, constrained by angles based on the spatial positions of the nodes. This parameter sets the minimum allowed angle for visiting nodes during the search. If the user-defined angle is lower than the calculated angle, the user-defined minimum angle is used.'),
         "Overlap": st.sidebar.slider(
-			'Overlap', 1, 10, 4, on_change=reset_session_state,
-			help="Defines the amount of a filament's length that can be shared between filaments. It's calculated as (defined filament)/overlap. For example, a value of 4 allows 1/4 of the path to be shared. Use higher values (less overlap) for systems with minimal overlap, and lower values for systems with more overlap. Experiments suggest 4 is optimal for actin."),
+            'Overlap', 1, 10, 4, on_change=reset_session_state,
+            help="Defines the amount of a filament's length that can be shared between filaments. It's calculated as (defined filament)/overlap. For example, a value of 4 allows 1/4 of the path to be shared. Use higher values (less overlap) for systems with minimal overlap, and lower values for systems with more overlap. Experiments suggest 4 is optimal for actin."),
         "Allowed Movement": st.sidebar.slider(
-			'Allowed Movement', 25, 200, 50, on_change=reset_session_state,
-			help="Time series parameter. Controls the maximum allowed distance filaments can move between frames. The distance is calculated as the Frobenius norm of the end node positions between potential matches. Lower values restrict movement to small perturbations between frames, while higher values allow for greater movement. This depends on your image data's dynamics and time interval. We recommend starting at 50."),
+            'Allowed Movement', 25, 200, 50, on_change=reset_session_state,
+            help="Time series parameter. Controls the maximum allowed distance filaments can move between frames. The distance is calculated as the Frobenius norm of the end node positions between potential matches. Lower values restrict movement to small perturbations between frames, while higher values allow for greater movement. This depends on your image data's dynamics and time interval. We recommend starting at 50."),
         "Merge Radius (Size)": st.sidebar.select_slider(
-            'Merge Radius (Size)', 
-            options=[i for i in range(2, 21, 2)], 
-            value=6, 
-            on_change=reset_session_state,        
-			help='Allows "melting" of nodes to compensate for potential errors in the segmentation step. Set this value based on the amount of noise and blur in your image. Use low values (2-6) for low-noise images, and higher values for noisier image data.'),
+            'Merge Radius (Size)',
+            options=[i for i in range(2, 21, 2)],
+            value=6,
+            on_change=reset_session_state,
+            help='Allows "melting" of nodes to compensate for potential errors in the segmentation step. Set this value based on the amount of noise and blur in your image. Use low values (2-6) for low-noise images, and higher values for noisier image data.'),
         "Bendiness": st.sidebar.slider('Bendiness', 1, 400, 200, on_change=reset_session_state,
-			help='Controls the addition of nodes to better express the "bendiness" of underlying filamentous structures. Set this value to reflect how bendy your filamentous structures are. Lower values add more nodes, while higher values add fewer nodes.'),
-        "Fine Structure Sensitivity": st.sidebar.slider('Fine Structure Sensitivity', 0.01, 0.7, 0.5, on_change=reset_session_state,        
-			help="Adjusts the lower value of the hysteresis threshold applied to the image data. This filter helps eliminate noise and uneven backgrounds. The upper threshold is calculated using Otsu's method. Use lower values (around 0.1) to capture fine filamentous structures, and higher values (around 0.5) for noisier images.")
+            help='Controls the addition of nodes to better express the "bendiness" of underlying filamentous structures. Set this value to reflect how bendy your filamentous structures are. Lower values add more nodes, while higher values add fewer nodes.'),
+        "Fine Structure Sensitivity": st.sidebar.slider('Fine Structure Sensitivity', 0.01, 0.7, 0.5, on_change=reset_session_state,
+            help="Adjusts the lower value of the hysteresis threshold applied to the image data. This filter helps eliminate noise and uneven backgrounds. The upper threshold is calculated using Otsu's method. Use lower values (around 0.1) to capture fine filamentous structures, and higher values (around 0.5) for noisier images.")
     }
 
     if uploaded_file is not None:
         st.session_state['params'] = params
         if st.button('Run Analysis'):
             run_analysis(uploaded_file, params)
+
+    if image_source == "Use example image":
+        uploaded_file.close()  # manually close the file if it's an example image
+
 
 if __name__ == "__main__":
     main()
